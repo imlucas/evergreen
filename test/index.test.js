68821c394b5b856acb3b0e61386359358cae20d4
Lucas Hrabovsky <hrabovsky.lucas@gmail.com>
2015-11-30 10:10:45 -0500

    update to latest evergreen cli release

--- test/index.test.js
+++ test/index.test.js
@@ -9,19 +9,19 @@ describe('evergreen', function() {
       it('should return the right download URL for `darwin`', function() {
         assert.equal(evergreen.client.url('darwin'),
           'https://s3.amazonaws.com/mciuploads/mci/cli/mci_osx_client_'
-          + '3010ffda7d09fe404360d6d59dc764bfcf784a59_15_09_17_17_56_12/evergreen'
+          + 'a9b788695b851f8395f562e81ace8e436f4e9591_15_11_10_21_07_55/evergreen'
         );
       });
       it('should return the right download URL for `linux`', function() {
         assert.equal(evergreen.client.url('linux'),
           'https://s3.amazonaws.com/mciuploads/mci/cli/mci_ubuntu_client_'
-          + '3010ffda7d09fe404360d6d59dc764bfcf784a59_15_09_17_17_56_12/evergreen'
+          + 'a9b788695b851f8395f562e81ace8e436f4e9591_15_11_10_21_07_55/evergreen'
         );
       });
       it('should return the right download URL for `win32`', function() {
         assert.equal(evergreen.client.url('win32'),
           'https://s3.amazonaws.com/mciuploads/mci/cli/mci_windows_64_client_'
-          + '3010ffda7d09fe404360d6d59dc764bfcf784a59_15_09_17_17_56_12/evergreen.exe'
+          + 'a9b788695b851f8395f562e81ace8e436f4e9591_15_11_10_21_07_55/evergreen.exe'
         );
       });
     });
